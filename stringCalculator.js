d3d193b59d876bb6d082b9c10dbed943e6c02d66
Ashwani Patel <patel1998ashwani@gmail.com>
2024-10-18 13:29:08 +0530

    Update stringCalculator.js
    updating the code show that it can accept two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,13 @@ function add(numbers) {
     if (numbers === "") {
       return 0;
     }
-    return parseInt(numbers , 10) // for handling single number
+  //  return parseInt(numbers , 10); // for handling single number
+
+
+    const nums= numbers.split(",");
+    return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);// for adding two numbers
+
   }
   console.log(add(""))    // output 0
-
+  console.log(add("1"));  // output 5
+  console.log(add("1,5")); // output 6

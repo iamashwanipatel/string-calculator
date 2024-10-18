8e4487232ed1a104e1bccc9c6de23cac33439bf7
Ashwani Patel <patel1998ashwani@gmail.com>
2024-10-18 13:55:34 +0530

    Update stringCalculator.js
    updating code to ignore number bigger than 1000

--- stringCalculator.js
+++ stringCalculator.js
@@ -18,8 +18,29 @@ const nums = numbers.split(delimiter);
     throw new Error(`negative numbers not allowed => ${negatives.join(", ")}`);
   }
 
-return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);
+// return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);
+
+// making changes show that this does not support number bigger that 1000
+return nums.reduce((sum, num) => {
+  const n = parseInt(num, 10);
+  return n > 1000 ? sum : sum + n;
+}, 0);
+
 }
+ 
+// Examples
+console.log(add(""));        // Output: 0
+console.log(add("1"));       // Output: 1
+console.log(add("1,5"));     // Output: 6
+console.log(add("1,2,3,4")); // Output: 10
+console.log(add("10,20,30")); // Output: 60
+console.log(add("1\n2,3"));  // Output: 6
+console.log(add("//;\n1;2"));  //Output :3
+
+// if number is bigger that 1000
+console.log(add("2,1001"));    // Output: 2 
+
+
 try{
     console.log(add("1,-2,3"));// it should throw error
 }
@@ -30,12 +51,4 @@ try {
     console.log(add("10,-20,-30")); // Should throw error
   } catch (e) {
     console.log(e.message); // negative numbers not allowed: -20, -30
-  }  
-// Examples
-console.log(add(""));        // Output: 0
-console.log(add("1"));       // Output: 1
-console.log(add("1,5"));     // Output: 6
-console.log(add("1,2,3,4")); // Output: 10
-console.log(add("10,20,30")); // Output: 60
-console.log(add("1\n2,3"));  // Output: 6
-console.log(add("//;\n1;2"));  //Output :3
+  } 

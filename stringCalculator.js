c742ce07b8c9d3bb3eb3b9e1cd05e9c2abbfd18e
Ashwani Patel <patel1998ashwani@gmail.com>
2024-10-18 13:32:37 +0530

    Update stringCalculator.js
    it will also accept any amount of numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,12 +3,14 @@ function add(numbers) {
       return 0;
     }
   //  return parseInt(numbers , 10); // for handling single number
+  
+const nums = numbers.split(",");
+return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);
+}
 
-
-    const nums= numbers.split(",");
-    return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);// for adding two numbers
-
-  }
-  console.log(add(""))    // output 0
-  console.log(add("1"));  // output 5
-  console.log(add("1,5")); // output 6
+// Examples
+console.log(add(""));        // Output: 0
+console.log(add("1"));       // Output: 1
+console.log(add("1,5"));     // Output: 6
+console.log(add("1,2,3,4")); // Output: 10
+console.log(add("10,20,30")); // Output: 60

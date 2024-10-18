68c9ea847e000a344da1e0d1948b90d0d42f5336
Ashwani Patel <patel1998ashwani@gmail.com>
2024-10-18 13:40:24 +0530

    Update stringCalculator.js
    updated add method to handle delimiters specified at the beginning of the string.

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,7 +4,14 @@ function add(numbers) {
     }
   //  return parseInt(numbers , 10); // for handling single number
   
-const nums = numbers.split(/[\n,]/);
+let delimiter = /[\n,]/;
+if (numbers.startsWith("//")) {
+  const parts = numbers.split("\n");
+  delimiter = new RegExp(parts[0].substring(2));
+  numbers = parts.slice(1).join("\n");
+}
+
+const nums = numbers.split(delimiter)
 return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
@@ -15,3 +22,4 @@ console.log(add("1,5"));     // Output: 6
 console.log(add("1,2,3,4")); // Output: 10
 console.log(add("10,20,30")); // Output: 60
 console.log(add("1\n2,3"));  // Output: 6
+console.log(add("//;\n1;2"));  //Output :3
